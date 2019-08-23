9f2836610c168debd0ff07480bb39dd2480bdb94
tassianunes <54067496+tassianunes@users.noreply.github.com>
2019-08-23 16:46:35 -0300

    Update IMC.py

--- IMC.py
+++ IMC.py
@@ -3,3 +3,7 @@ altura = 1.63
 imc = peso/(altura**2)
 print ('Seu IMC é:', IMC)
 print ("Muito abaixo do peso?", imc < 17.0)
+print ("Abaixo do peso normal?", imc >= 17.0 and imc <= 18.5)
+print ("Peso dentro do Normal?", imc > 18.5 and imc <= 25.0)
+print ("Acima do peso normal?", imc > 25.0 and imc <= 30.0)
+print ("Muito acima do peso?", imc > 30.0)
